3c4514ea90a500f3482162e9e6449a4921e92881
Fahmida Yasmin <fahmidayasmin099@gmail>
2025-10-04 02:25:54 +0600

    fixing wsgi.py

--- hsp/hsp/wsgi.py
+++ hsp/hsp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hsp.hsp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hsp.settings')
 
 application = get_wsgi_application()

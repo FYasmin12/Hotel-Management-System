8126bdba768120e968dabc9116b81f04deba35b4
Fahmida Yasmin <fahmidayasmin099@gmail>
2025-10-03 22:24:29 +0600

    solved errors

--- hsp/hsp/settings.py
+++ hsp/hsp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os 
+from decouple import config # pyright: ignore[reportMissingImports]
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,8 +28,11 @@ DEBUG = True
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'fahmidayasmin099@gmail.com'
-EMAIL_HOST_PASSWORD = 'flif trqv agll dtgs'
+
+
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
+
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
